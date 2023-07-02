232c017fa12f713b6f03ca9a324c4bf6e4ee93be
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2023-07-02 21:42:00 +0300

    First launch view 1.0v

--- src/main/java/com/example/fin/controllers/FirstLaunchController.java
+++ src/main/java/com/example/fin/controllers/FirstLaunchController.java
@@ -1,23 +1,25 @@
 package com.example.fin.controllers;
 
 import com.example.fin.MainApplication;
+import com.example.fin.utils.FileUtils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import org.controlsfx.dialog.ExceptionDialog;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class FirstLaunchController {
     private static final String regexNum = "\\d+[.|,]*\\d*";
-    private static final String regexDate = "\\d{1,2}";
-    private static final String PATH = "/data.txt";
+    private static final String regexDate = "[1-31]{1,2}";
     @FXML
     private TextField salaryText;
     @FXML
@@ -27,23 +29,26 @@ public class FirstLaunchController {
     @FXML
     private TextField minSumText;
     @FXML
+    TextField startSumText;
+    @FXML
     private Button okBtn;
 
     @FXML
-    private void onOkClick(MouseEvent mouseEvent) {
+    private void onOkClick(ActionEvent actionEvent) {
         String message;
         if (!checkFields()) {
             message = "Поля неверно заполнены";
             showEx(message);
             return;
         }
-        String salary = salaryText.getText().replaceAll("\s", "");
-        String percent = percentText.getText().replaceAll("\s", "");
-        String date = dateText.getText().replaceAll("\s", "");
-        String minVal = minSumText.getText().replaceAll("\s", "");
+        String salary = salaryText.getText().replaceAll(" ", "");
+        String percent = percentText.getText().replaceAll(" ", "");
+        String date = dateText.getText().replaceAll(" ", "");
+        String minVal = minSumText.getText().replaceAll(" ", "");
+        String startSum = startSumText.getText().replaceAll(" ", "");
 
         if (salary.matches(regexNum) && percent.matches(regexNum) && date.matches(regexDate)) {
-            saveData(salary, percent, date, minVal);
+            saveData(salary, percent, date, minVal, startSum);
             //Switch to another screen
             try {
                 Stage stage = (Stage) okBtn.getScene().getWindow();
@@ -60,6 +65,7 @@ public class FirstLaunchController {
         }
     }
 
+    //TODO: добавить сохранение зарплаты
     /**
      * Save user's data in txt file
      *
@@ -68,15 +74,33 @@ public class FirstLaunchController {
      * @param date user's pay day
      * @param minVal minimum number for a pillow
      */
-    private void saveData(String salary, String percent, String date, String minVal) {
-        try (FileOutputStream writer = new FileOutputStream(new File(PATH))) {
-            writer.write((salary + " ").getBytes());
-            writer.write((percent + " ").getBytes());
+    private void saveData(String salary, String percent, String date, String minVal, String startSum) {
+        try (FileOutputStream writer = new FileOutputStream(new File(FileUtils.PATH))) {
+            if (!startSum.isEmpty())
+                writer.write((startSum + " ").getBytes());
+            else
+                writer.write("0 ".getBytes());
+            date = validateDate(date);
             writer.write((date + " ").getBytes());
+            writer.write((percent + " ").getBytes());
             writer.write(minVal.getBytes());
+            //writer.write((salary + " ").getBytes());
         } catch (Exception ex) {
+            System.out.println("saveData " + ex);
             new ExceptionDialog(ex);
         }
+    }
+
+    private String validateDate(String date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        Calendar today = Calendar.getInstance();
+        Calendar payDay = Calendar.getInstance();
+        payDay.set(Calendar.DAY_OF_MONTH, Integer.parseInt(date));
+        if (today.before(payDay))
+            return sdf.format(payDay.getTime());
+        // TODO: Может убрать это и оставить только для текущего месяца
+        payDay.add(Calendar.MONTH, 1);
+        return sdf.format(payDay);
     }
 
     /**
