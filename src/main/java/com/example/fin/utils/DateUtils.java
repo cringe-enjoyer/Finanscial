d84c4057c7b78deb7fd359859cfb4f160a552c78
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2023-07-05 13:50:56 +0300

    Add utils for dates

---
+++ src/main/java/com/example/fin/utils/DateUtils.java
@@ -0,0 +1,34 @@
+package com.example.fin.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DateUtils {
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+    private static final SimpleDateFormat sdfAmer = new SimpleDateFormat("yyyy.MM.dd");
+    public static Calendar stringToDate(String strDate) throws ParseException {
+        Calendar date = Calendar.getInstance();
+        date.setTime(sdf.parse(strDate));
+        return date;
+    }
+
+
+    /**
+     * Convert date to string (dd.MM.yyyy)
+     * @param date date to convert
+     * @return string date
+     */
+    public static String dateToString(Calendar date) {
+        return sdf.format(date.getTime());
+    }
+
+    /**
+     * Convert date to string in American format (yyyy.MM.dd)
+     * @param date date to convert
+     * @return string date in American format
+     */
+    public static String dateToStringAmer(Calendar date) {
+        return sdfAmer.format(date.getTime());
+    }
+}
