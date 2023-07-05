582c87a2fa673db0839b8e8d1bef194216b94e1c
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2023-07-05 13:50:09 +0300

    Add sqlite

---
+++ src/main/java/com/example/fin/database/Sqlite.java
@@ -0,0 +1,95 @@
+package com.example.fin.database;
+
+import com.example.fin.Pillow;
+import com.example.fin.utils.DateUtils;
+
+import java.sql.*;
+
+public class Sqlite {
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:sqlite:src/main/resources/finanscial.sqlite");
+    }
+
+    public static boolean addPillow(Pillow pillow) {
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+            statement.executeUpdate("INSERT INTO user_data (salary, sum, percent, update_date, min_value) VALUES" +
+                    "(" + pillow.getSalary() + ", " + pillow.getSum() + ", " + pillow.getPercent() + ", '"
+                    + DateUtils.dateToString(pillow.getUpdateDay()) + "', " + pillow.getMinVal() + ");");
+            connection.close();
+        } catch (Exception exception) {
+            System.out.println(exception);
+            return false;
+        }
+        return true;
+    }
+
+    public static Pillow getPillow() {
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+            ResultSet result = statement.executeQuery("SELECT * from user_data");
+            Pillow pillow = null;
+            if (result.next()) {
+                pillow = new Pillow(result.getDouble("sum"), result.getString("update_date"),
+                        result.getDouble("percent"), result.getInt("min_value"),
+                        result.getDouble("salary"));
+            }
+            statement.close();
+            connection.close();
+            return pillow;
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void updatePillow(Pillow pillow) {
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+            statement.executeUpdate("UPDATE user_data SET salary = " + pillow.getSalary() + ", sum = "
+                    + pillow.getSum() + ", percent = " + pillow.getPercent() + ", update_date = '" +
+                    DateUtils.dateToString(pillow.getUpdateDay()) + "', min_value = " + pillow.getMinVal() + ";");
+        } catch (Exception exception) {
+
+        }
+    }
+
+    public static void checkDB() {
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT tbl_name FROM sqlite_master WHERE tbl_name LIKE 'user_data';");
+            if (resultSet.next()) {
+                if (resultSet.getString("tbl_name").equals("user_data"))
+                    return;
+            }
+            createDB();
+        } catch (Exception ex) {
+
+        }
+    }
+
+    private static void createDB() {
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            statement.executeQuery("create table user_data ( " +
+                    "    id          INTEGER" +
+                    "        constraint user_data_pk" +
+                    "            primary key autoincrement," +
+                    "    salary      REAL," +
+                    "    sum         REAL," +
+                    "    percent     REAL," +
+                    "    update_date TEXT," +
+                    "    min_value   INTEGER);");
+        } catch (Exception exception) {
+
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module com.example.fin {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
     opens com.example.fin to javafx.fxml;
     exports com.example.fin;
