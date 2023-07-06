4aec548f50f68b3d9b625306b63a27a1bef4580f
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2023-07-06 11:03:01 +0300

    Refactor with sqlite support

--- src/main/java/com/example/fin/MainApplication.java
+++ src/main/java/com/example/fin/MainApplication.java
@@ -1,18 +1,17 @@
 package com.example.fin;
 
-import com.example.fin.utils.FileUtils;
+import com.example.fin.database.Sqlite;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.File;
 import java.io.IOException;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        if (checkFile()) {
+        if (checkPillow()) {
             FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("firstLaunch-view.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
             stage.setTitle("Financial pillow");
@@ -28,9 +27,9 @@ public class MainApplication extends Application {
         }
     }
 
-    private boolean checkFile() {
-        File file = new File(FileUtils.PATH);
-        return !file.exists() || FileUtils.isEmpty(file);
+    private boolean checkPillow() {
+        Sqlite.checkDB();
+        return Sqlite.getPillow() == null;
     }
 
     public static void main(String[] args) {
